bb84f7331a896d7e5d30c9b1deef9288ee431029
Dereck Wonnacott <dereck@gmail.com>
2021-02-28 09:35:27 -0500

    Fix build failure on Linux

--- plugins/DataLoadRobotLog/dataload_robot_log.cpp
+++ plugins/DataLoadRobotLog/dataload_robot_log.cpp
@@ -47,8 +47,7 @@ bool DataLoadRobotLog::readDataFromFile(FileLoadInfo* info, PlotDataMapRef& plot
     quint32 len;
 
     inB >> len;
-    // len = _byteswap_ulong(len);
-    len = __builtin_bswap32(len);
+    len = FLATBUFFERS_BYTESWAP32(len);
     char frame_bytes[len];
 
     inB.readRawData(frame_bytes, len);
